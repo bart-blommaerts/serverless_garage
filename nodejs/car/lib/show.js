31d6b2c64306b8491bc2656f6b8badd01f1434bf
Bart Blommaerts <bartblommaerts@ORD02261.local>
2016-02-26 12:15:11 +0100

    show distinct car from db

---
+++ nodejs/car/lib/show.js
@@ -0,0 +1,19 @@
+var dynamo = require('./dynamo');
+
+module.exports.respond = function (event, callback) {
+  var params = {
+    TableName: dynamo.tableName,
+    Key: {
+      id: event.id
+    }
+};
+  return dynamo.doc.get(params, function (error, data) {
+    if (error) {
+      callback(error);
+    } else {
+      var item = data.Item;
+      var newData = { 'car' : item };
+      callback(error, newData);
+    }
+  });
+};
