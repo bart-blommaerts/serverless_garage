da6ac1e05d15ece675e559502f850db99a7b91cb
Bart Blommaerts <bartblommaerts@ORD02261.local>
2016-02-26 11:22:28 +0100

    added getting cars from db

---
+++ nodejs/car/lib/index.js
@@ -0,0 +1,15 @@
+var dynamo = require('./dynamo');
+module.exports.respond = function (event, callback) {
+  var params = {
+    TableName: dynamo.tableName,
+};
+  return dynamo.doc.scan(params, function (error, data) {
+    if (error) {
+      callback(error);
+    } else {
+      var items = data.Items;
+      var newData = { 'car' : items };
+      callback(error, newData);
+    }
+  });
+};
